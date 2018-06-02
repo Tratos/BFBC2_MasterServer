bc2a6020391e0f1ce8685e57cb155e6288864980
Marek Grzyb <b1narykill3r@gmail.com>
2018-06-02 16:55:28 +0200

    Plasma packets that are encrypted are now being handled correctly

--- Network/PlasmaServer.py
+++ Network/PlasmaServer.py
@@ -1,3 +1,5 @@
+from base64 import b64decode
+
 from twisted.internet.protocol import Protocol
 
 from DataClasses import Server
@@ -12,6 +14,8 @@ class HANDLER(Protocol):
         self.CONNOBJ = None
         self.logger = Log("PlasmaServer", "\033[36;1m")
         self.logger_err = Log("PlasmaServer", "\033[36;1;41m")
+
+        self.packetData = ""
 
     def connectionMade(self):
         self.ip, self.port = self.transport.client
@@ -37,7 +41,7 @@ class HANDLER(Protocol):
 
     def dataReceived(self, data):
         packet_type = data[:4]
-        packet_checksum = data.split(packet_type)[1].split("TXN")[0]
+        packet_checksum = data.split(data[12:])[0].split(packet_type)[1]
         packet_id = Packet(None).getPacketID(packet_checksum[:4])
         packet_length = packet_checksum[4:]
         packet_data = data.split(packet_type + packet_checksum)[1]
@@ -51,7 +55,21 @@ class HANDLER(Protocol):
         else:
             self.CONNOBJ.plasmaPacketID += 1
 
-        if Packet(data).verifyPacketLength(packet_length):
+        try:
+            dataEncrypted = dataObj.get("PacketData", "data")
+
+            self.packetData += dataEncrypted.replace("%3d", "=")
+
+            if len(self.packetData) == int(dataObj.get("PacketData", "size")):
+                dataObj = Packet(b64decode(self.packetData) + "\x00").dataInterpreter()
+                self.packetData = ""
+                isValidPacket = True
+            else:
+                isValidPacket = False
+        except:
+            isValidPacket = True
+
+        if Packet(data).verifyPacketLength(packet_length) and isValidPacket:
             TXN = dataObj.get("PacketData", "TXN")
 
             if packet_type == "fsys":
@@ -63,6 +81,8 @@ class HANDLER(Protocol):
             else:
                 self.logger_err.new_message(
                     "[" + self.ip + ":" + str(self.port) + ']<-- Got unknown message type (' + packet_type + ")", 2)
+        elif not isValidPacket:
+            pass
         else:
             self.logger_err.new_message("Warning: Packet Length is diffirent than the received data length!"
                                         "(" + self.ip + ":" + self.port + "). Ignoring that packet...", 2)
