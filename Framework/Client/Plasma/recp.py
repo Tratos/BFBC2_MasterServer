e86322d1ada3de660f5dccdee06fb787613dfa54
Marek Grzyb <b1narykill3r@gmail.com>
2018-06-02 11:52:28 +0200

    Added GetRecord handler

--- Framework/Client/Plasma/recp.py
+++ Framework/Client/Plasma/recp.py
@@ -17,9 +17,23 @@ def HandleGetRecordAsMap(self):
 
     Packet(newPacket).sendPacket(self, "rank", 0x80000000, self.CONNOBJ.plasmaPacketID)
 
+def HandleGetRecord(self):
+    # TODO: find out what it is, and what to do with it
+
+    newPacket = ConfigParser()
+    newPacket.optionxform = str
+    newPacket.add_section("PacketData")
+    newPacket.set("PacketData", "TXN", "GetRecord")
+    newPacket.set("PacketData", "localizedMessage", "Record not found")
+    newPacket.set("PacketData", "errorContainer.[]", "0")
+    newPacket.set("PacketData", "errorCode", "5000")
+    Packet(newPacket).sendPacket(self, "rank", 0x80000000, self.CONNOBJ.plasmaPacketID)
+
 
 def ReceivePacket(self, data, txn):
     if txn == 'GetRecordAsMap':
         HandleGetRecordAsMap(self)
+    elif txn == 'GetRecord':
+        HandleGetRecord(self)
     else:
         self.logger_err.new_message("[" + self.ip + ":" + str(self.port) + ']<-- Got unknown recp message (' + txn + ")", 2)
