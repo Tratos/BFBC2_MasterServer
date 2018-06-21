ccace7dce1c19d4ecb93edbae3c337b7276e4803
Marek Grzyb <b1narykill3r@gmail.com>
2018-06-21 19:52:10 +0200

    Fixed asso packet responses in PlasmaServer

--- Framework/Server/Plasma/asso.py
+++ Framework/Server/Plasma/asso.py
@@ -6,29 +6,45 @@ def HandleGetAssociations(self, data):
     toSend.set("PacketData", "TXN", "GetAssociations")
 
     type = data.get("PacketData", "type")
-    toSend.set("PacketData", "type", type)
+
     toSend.set("PacketData", "domainPartition.domain", data.get("PacketData", "domainPartition.domain"))
     toSend.set("PacketData", "domainPartition.subDomain", data.get("PacketData", "domainPartition.subDomain"))
     toSend.set("PacketData", "owner.id", str(self.CONNOBJ.personaID))
-    toSend.set("PacketData", "owner.name", self.CONNOBJ.personaName)
     toSend.set("PacketData", "owner.type", "1")
+    toSend.set("PacketData", "type", type)
+    toSend.set("PacketData", "members.[]", "0")
 
     if type == "PlasmaMute":
-        # TODO: Make the mute list database
         toSend.set("PacketData", "maxListSize", "20")
-        toSend.set("PacketData", "members.[]", "0")
+        toSend.set("PacketData", "owner.name", self.CONNOBJ.personaName)
     elif type == 'PlasmaBlock':
-        # TODO: Make the block list database
         toSend.set("PacketData", "maxListSize", "20")
-        toSend.set("PacketData", "members.[]", "0")
+        toSend.set("PacketData", "owner.name", self.CONNOBJ.personaName)
     elif type == 'PlasmaFriends':
-        # TODO: Make the friends list database
         toSend.set("PacketData", "maxListSize", "20")
-        toSend.set("PacketData", "members.[]", "0")
+        toSend.set("PacketData", "owner.name", self.CONNOBJ.personaName)
     elif type == 'PlasmaRecentPlayers':
-        # TODO: Make the recent list database
         toSend.set("PacketData", "maxListSize", "100")
-        toSend.set("PacketData", "members.[]", "0")
+    elif type == 'dogtags':
+        toSend.set("PacketData", "maxListSize", "20")
+        toSend.set("PacketData", "owner.name", self.CONNOBJ.personaName)
+
+    Packet(toSend).send(self, "asso", 0x80000000, self.CONNOBJ.plasmaPacketID)
+
+
+def HandleAddAssociations(self, data):
+    toSend = Packet().create()
+    toSend.set("PacketData", "TXN", "AddAssociations")
+
+    type = data.get("PacketData", "type")
+
+    toSend.set("PacketData", "domainPartition.domain", data.get("PacketData", "domainPartition.domain"))
+    toSend.set("PacketData", "domainPartition.subDomain", data.get("PacketData", "domainPartition.subDomain"))
+    toSend.set("PacketData", "type", type)
+    toSend.set("PacketData", "result.[]", "0")
+
+    if type == 'PlasmaRecentPlayers':
+        toSend.set("PacketData", "maxListSize", "100")
 
     Packet(toSend).send(self, "asso", 0x80000000, self.CONNOBJ.plasmaPacketID)
 
@@ -36,5 +52,7 @@ def HandleGetAssociations(self, data):
 def ReceivePacket(self, data, txn):
     if txn == "GetAssociations":
         HandleGetAssociations(self, data)
+    elif txn == "AddAssociations":
+        HandleAddAssociations(self, data)
     else:
         self.logger_err.new_message("[" + self.ip + ":" + str(self.port) + ']<-- Got unknown asso message (' + txn + ")", 2)
